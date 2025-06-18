f2cf33aa9f853beb4bd87adee227c25a4584f277
Michelle <michelle@Michelles-MacBook-Air.local>
2025-06-18 17:13:01 -0400

    rename project

--- fitness-tracker-api/src/main/java/com/fitnessapi/fitness_tracker_api/FitnessTrackerApiApplication.java
+++ fitness-tracker-api/src/main/java/com/fitnessapi/fitness_tracker_api/FitnessTrackerApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.fitnessapi.fitness_tracker_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FitnessTrackerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FitnessTrackerApiApplication.class, args);
 	}
 
 }
--- fitness-tracker-api/src/test/java/com/fitnessapi/fitness_tracker_api/FitnessTrackerApiApplicationTests.java
+++ fitness-tracker-api/src/test/java/com/fitnessapi/fitness_tracker_api/FitnessTrackerApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.fitnessapi.fitness_tracker_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FitnessTrackerApiApplicationTests {
 
 	@Test
 	void contextLoads() {
